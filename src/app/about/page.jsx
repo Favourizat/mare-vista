6fe4ce77104091d60298eac677cbeb289262becd
unknown <iwuohafavour06@gmail git config --global user.name Favourizat.com>
2026-09-25 19:29:10 +0100

    Update service detail pages

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -284,7 +284,7 @@ export default function AboutPage() {
 
                     <div className="relative min-h-[550px] lg:min-h-[650px]">
                         <Image
-                            src="/rooms/standard-room.jpg"
+                            src="/rooms/Tripple-classic.jpg"
                             alt="Mare Vista luxury room"
                             fill
                             className="object-contain"
